81484c336d5731425d132a31d1e38e558d58abd4
dxxdev <diyorbekxojamberdiyevn1@gmail.com>
2024-01-28 17:48:54 +0500

    Added react-router-dom

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "../components/Header";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+
+const MainLayout = () => {
+  return (
+    <div className="flex flex-col justify-between min-h-screen">
+      <Header />
+      <main className="grow">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default MainLayout;
