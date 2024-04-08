284709ed363be892a113c6046dce797add0e9dac
dxxdev <diyorbekxojamberdiyevn1@gmail.com>
2024-04-09 04:56:29 +0500

    Added product price section on detail page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -31,18 +31,17 @@ const ProductDetail = () => {
   };
 
   return (
-    <div className="my-container space-y-5 py-14">
+    <div className="my-container space-y-10 !py-4">
       {product && (
         <h2 className="font-bold text-3xl leading-120">
           {product.productName}{" "}
-          {product.property[2].types[product.property[2].typeIndex]}{" "}
-          {product?.discountPrice()}
+          {product.property[2].types[product.property[2].typeIndex]}
         </h2>
       )}
       {product && (
         <div className="flex flex-col items-start p-0 gap-30">
           <div className="flex flex-row w-full items-start p-0 gap-10">
-            <div className="flex flex-col sticky top-5 w-full h-max items-center justify-center p-0 gap-5">
+            <div className="flex flex-col sticky top-5 w-max h-max items-center justify-center p-0 gap-5">
               <div className="slider-container p-3 shadow-menu-shadow rounded-3xl w-80 h-430">
                 <Slider {...settings} className="h-full">
                   {product &&
@@ -72,6 +71,7 @@ const ProductDetail = () => {
                             className="w-full !bg-medium-gray"
                             onChange={(value) => {
                               prop.typeIndex = value;
+                              setRendered((prev) => !prev);
                             }}
                             options={prop.types.map((type, index) => {
                               return {
@@ -80,32 +80,56 @@ const ProductDetail = () => {
                               };
                             })}
                           />
-                          {/* {prop.types &&
-                            prop.types.map((type, index) => {
-                              return (
-                                <li
-                                  key={index}
-                                  className={`px-4 cursor-pointer py-3 rounded-[10px] shadow-product-card-shadow w-full ${
-                                    index == prop.typeIndex
-                                      ? "bg-bright-green text-white"
-                                      : "bg-transparent text-dark-gray"
-                                  }`}
-                                  onClick={() => {
-                                    prop.typeIndex = index;
-                                    setRendered((prev) => !prev);
-                                  }}
-                                >
-                                  {type}
-                                </li>
-                              );
-                            })} */}
                         </ul>
                       </div>
                     </div>
                   );
                 })}
             </div>
-            <div className="flex w-full flex-col items-start p-0 gap-30"></div>
+            <div className="flex w-full flex-col items-start p-0 gap-30">
+              <div className="p-6 shadow rounded w-full">
+                <div className="flex flex-col justify-between gap-5">
+                  <div className="flex justify-start gap-2.5">
+                    <p className="text-dark-gray text-base font-medium leading-152">
+                      Naxi:
+                    </p>
+                    <b className="text-dark-gray text-2xl leading-normal font-bold">
+                      {product?.discountPrice()} $
+                    </b>
+                  </div>
+                  <div className="flex justify-start items-center">
+                    <button
+                      onClick={() => {
+                        product.saved = !product.saved;
+                        setRendered((prev) => !prev);
+                      }}
+                      className="flex justify-center items-center gap-2.5"
+                    >
+                      <span className="text-sm font-normal text-dark-gray">
+                        Sevimlilarga
+                      </span>
+                      <span>
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="18"
+                          height="17"
+                          viewBox="0 0 22 21"
+                          fill={product?.saved ? "#00C65E" : "none"}
+                        >
+                          <path
+                            d="M11.62 19.2998C11.28 19.4198 10.72 19.4198 10.38 19.2998C7.48 18.3098 1 14.1798 1 7.17984C1 4.08984 3.49 1.58984 6.56 1.58984C8.38 1.58984 9.99 2.46984 11 3.82984C12.01 2.46984 13.63 1.58984 15.44 1.58984C18.51 1.58984 21 4.08984 21 7.17984C21 14.1798 14.52 18.3098 11.62 19.2998Z"
+                            stroke="#00C65E"
+                            strokeWidth="1.5"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                          />
+                        </svg>
+                      </span>
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       )}
