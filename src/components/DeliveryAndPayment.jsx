af3f623d88b2cbbada05598de9be0b2bda258b83
dxxdev <diyorbekxojamberdiyevn1@gmail.com>
2024-04-17 16:36:07 +0500

    Tab buttons added

--- src/components/DeliveryAndPayment.jsx
+++ src/components/DeliveryAndPayment.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const DeliveryAndPayment = () => {
   return (
-    <div>DeliveryAndPayment</div>
-  )
-}
+    <div className="flex flex-col gap-10 justify-start items-start">
+      <div className="flex justify-between items-center gap-5">
+        <button className="btn text-white">Yetkazib berish</button>
+        <button className="btn text-bright-green !bg-white ">
+          Topshirish punktlari
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default DeliveryAndPayment
+export default DeliveryAndPayment;
