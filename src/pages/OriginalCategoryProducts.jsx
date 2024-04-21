e573af34533f5e272d39518379ffed04b34364e7
dxxdev <diyorbekxojamberdiyevn1@gmail.com>
2024-04-21 16:00:53 +0500

    Added brand selection to sort by brand

--- src/pages/OriginalCategoryProducts.jsx
+++ src/pages/OriginalCategoryProducts.jsx
@@ -3,7 +3,7 @@ import { useLocation } from "react-router-dom";
 import filteredOriginalCategory from "../functions/filteredOriginalCategory";
 import { products } from "../data/data";
 import filteredBrend from "../functions/filteredBrend";
-import { InputNumber, Slider } from "antd";
+import { Checkbox, InputNumber, Slider } from "antd";
 import filteredPrice from "../functions/filteredPrice";
 
 const OriginalCategoryProducts = () => {
@@ -12,6 +12,8 @@ const OriginalCategoryProducts = () => {
   const [brends, setBrends] = useState([]);
   const [maxPrice, setMaxPrice] = useState(0);
   const [minPrice, setMinPrice] = useState(0);
+  const [openBrend, setOpenBrend] = useState(true);
+  let checkedBrends = [];
   useEffect(() => {
     setBrends(filteredBrend(filteredOriginalCategory(products, pathname)));
     setMaxPrice(
@@ -65,6 +67,64 @@ const OriginalCategoryProducts = () => {
                     max={maxPrice}
                     defaultValue={[minPrice, maxPrice]}
                   />
+                </div>
+                <div className="flex flex-col justify-start items-start gap-4 w-full">
+                  <button
+                    onClick={() => {
+                      setOpenBrend((prev) => !prev);
+                    }}
+                    className="flex justify-between items-center w-full"
+                  >
+                    <span className="text-base font-medium text-dark-gray leading-152">
+                      Brend
+                    </span>
+                    <span>
+                      <svg
+                        width="10"
+                        height="6"
+                        viewBox="0 0 10 6"
+                        fill="none"
+                        className={`${openBrend ? "rotate-180" : "rotate-0"}`}
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M4.29728 0.689164L0.473393 4.38293C0.209396 4.63795 0.209386 5.05144 0.473385 5.30646C0.737384 5.56149 1.16542 5.56148 1.42942 5.30645L4.77522 2.07439L8.12136 5.30678C8.38536 5.56181 8.81339 5.5618 9.07739 5.30678C9.34139 5.05175 9.3414 4.63827 9.0774 4.38324L5.26885 0.704169C5.26384 0.699003 5.25853 0.694201 5.25331 0.689157C5.02232 0.466337 4.66555 0.438241 4.40331 0.605499C4.36581 0.629425 4.33029 0.657276 4.29728 0.689164Z"
+                          fill="#363A45"
+                        />
+                      </svg>
+                    </span>
+                  </button>
+                  {openBrend && (
+                    <ul className="flex flex-col justify-start items-start gap-2.5">
+                      {brends &&
+                        brends.length > 0 &&
+                        brends.map((brend, index) => {
+                          return (
+                            <li
+                              className="flex justify-start items-center gap-4"
+                              key={index}
+                            >
+                              <Checkbox
+                                onChange={() => {
+                                  if (!checkedBrends.includes(brend)) {
+                                    checkedBrends.push(brend);
+                                  } else {
+                                    checkedBrends = checkedBrends.filter(
+                                      (item) => item != brend
+                                    );
+                                  }
+                                  console.log(checkedBrends);
+                                }}
+                              >
+                                {brend}
+                              </Checkbox>
+                            </li>
+                          );
+                        })}
+                    </ul>
+                  )}
                 </div>
               </div>
             </div>
