0245907aa0c0d1f898f81e2949c022404f7ce302
dxxdev <diyorbekxojamberdiyevn1@gmail.com>
2024-04-21 14:00:03 +0500

    Added min price and max price rangebar

--- src/pages/OriginalCategoryProducts.jsx
+++ src/pages/OriginalCategoryProducts.jsx
@@ -3,13 +3,23 @@ import { useLocation } from "react-router-dom";
 import filteredOriginalCategory from "../functions/filteredOriginalCategory";
 import { products } from "../data/data";
 import filteredBrend from "../functions/filteredBrend";
+import { InputNumber, Slider } from "antd";
+import filteredPrice from "../functions/filteredPrice";
 
 const OriginalCategoryProducts = () => {
   const location = useLocation();
   const pathname = location.pathname.split("/").slice(1).pop();
   const [brends, setBrends] = useState([]);
+  const [maxPrice, setMaxPrice] = useState(0);
+  const [minPrice, setMinPrice] = useState(0);
   useEffect(() => {
     setBrends(filteredBrend(filteredOriginalCategory(products, pathname)));
+    setMaxPrice(
+      Math.max(...filteredPrice(filteredOriginalCategory(products, pathname)))
+    );
+    setMinPrice(
+      Math.min(...filteredPrice(filteredOriginalCategory(products, pathname)))
+    );
   }, []);
   return (
     <div className="my-container">
@@ -31,6 +41,30 @@ const OriginalCategoryProducts = () => {
                 );
               })}
           </ul>
+        </div>
+        <div className="flex justify-start items-start gap-30">
+          <div className="flex flex-col justify-center shadow-product-card-shadow items-center w-full max-w-72 gap-30">
+            <div className="flex flex-col justify-center items-start gap-30 px-30 py-6">
+              <div className="flex flex-col justify-start items-start gap-4">
+                <h6 className="text-base leading-152 font-medium text-dark-gray">
+                  Narxi:
+                </h6>
+                <div className="flex flex-col justify-center items-center gap-2">
+                  <div className="flex justify-center items-center gap-30">
+                    <InputNumber value={minPrice} placeholder="MIN" />
+                    <InputNumber value={maxPrice} placeholder="MAX" />
+                  </div>
+                  <Slider
+                    className="w-full"
+                    range
+                    min={minPrice}
+                    max={maxPrice}
+                    defaultValue={[minPrice, maxPrice]}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
