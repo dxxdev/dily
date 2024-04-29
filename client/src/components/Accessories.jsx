e51d94098c77696316cc19f806c76131d630279c
dxxdev <diyorbekxojamberdiyevn1@gmail.com>
2024-04-29 14:19:28 +0500

    Added a button named "All".

--- client/src/components/Accessories.jsx
+++ client/src/components/Accessories.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function Accessories() {
   return (
-    <div className='flex flex-col justify-start items-start gap-30'>
-      <div className='flex justify-start items-center gap-4'>
-        Accessories
+    <div className="flex flex-col justify-start items-start gap-30">
+      <div className="flex justify-start items-center gap-4">
+        <button className="flex justify-center items-center rounded-[10px] px-4 py-3 shadow-product-card-shadow bg-bright-green font-medium text-xs leading-140 text-white">
+          Barchasi
+        </button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Accessories
+export default Accessories;
