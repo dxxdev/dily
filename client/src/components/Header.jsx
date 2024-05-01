d01c735220536ca51c9af63c59e84199125be410
silvante <khamidov.ko@gmail.com>
2024-05-01 22:22:50 +0500

    fixing

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,12 +7,12 @@ import account_icon from '../assets/icons/profile-icon.svg'
 const Header = ({ top, setTop, one }) => {
 
   return (
-    <header className="bg-light-gray/80 backdrop-blur-md shadow-sm sticky top-0 z-[9999] py-3">
+    <header className="bg-light-gray/80 backdrop-blur-md shadow-sm sticky top-0 z-[9999] py-1">
       <div className="my-container">
         <div className="flex w-full items-center justify-between">
           <Link>
-            <h1 className="flex items-center gap-1 h-30 font-open-sans">
-              <img src={logo} alt="" className="h-full" />
+            <h1 className="flex items-center gap-1 font-open-sans">
+              <img src={logo} alt="logo" className="h-full" width={"100px"}/>
             </h1>
           </Link>
 
@@ -20,7 +20,7 @@ const Header = ({ top, setTop, one }) => {
             <ul className="flex items-center justify-between gap-10">
               <li className="lowercase first-letter:uppercase">
                 <NavLink to='/'>
-                  Internet-magazin Dily.uz
+                  dily.uz
                 </NavLink>
               </li>
               <li className="lowercase first-letter:uppercase">
