868de45ff34e178094e1bc9cb6a8d862ca57d802
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2024-05-02 15:26:09 +0500

    page not found page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,7 @@ import OriginalCategoryProducts from "./pages/OriginalCategoryProducts";
 import Save from "./pages/Save";
 import { products } from "./data/data";
 import Administration from "./pages/Administration";
+import PageNotFound from "./pages/PageNotFound";
 
 const App = () => {
   const [one, setone] = useState(0);
@@ -47,6 +48,7 @@ const App = () => {
         />
         <Route path="/saved" element={<Save one={one} setone={setone} />} />
         <Route path="/dash" element={<Administration />} />
+        <Route path="/*" element={<PageNotFound one={one} />} />
       </Route>
     )
   );
---
+++ client/src/pages/PageNotFound.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import page_not_found_img from '../assets/other-icon/page-not-found-img.png'
+import Header from '../components/Header'
+import CategoryNavLinks from '../components/CategoryNavLinks'
+import greenLine from '../assets/other-icon/line-green.svg'
+import orangeLine from '../assets/other-icon/line-orange.svg'
+const PageNotFoun = ({ one }) => {
+    return (
+        <div className='fixed w-full h-full bg-white top-40 z-[9999]'>
+            <div className="my-container flex flex-col items-center justify-evenly gap-20 relative">
+                <img className='absolute bottom-0 right-0 w-32' src={greenLine} alt="" />
+                <img className='absolute top-0 left-20 w-40' src={orangeLine} alt="" />
+                <img className='absolute top-0 right-0 w-10' src={greenLine} alt="" />
+                <img className='absolute bottom-0 w-20 rotate-90 left-0' src={orangeLine} alt="" />
+                <img className='absolute bottom-0 left-1/4 w-40' src={greenLine} alt="" />
+                <img className='absolute top-20 right-0 w-40' src={orangeLine} alt="" />
+                <h1 className='text-2xl text-bright-green font-bold font-open-sans'>Sahifa Topilmadi</h1>
+                <img src={page_not_found_img} alt="page not found img" />
+                <Link to='/' className='px-10 py-3 rounded-xl bg-bright-green text-white'>Bosh sahifaga o'tish</Link>
+            </div>
+        </div>
+    )
+}
+
+export default PageNotFoun
