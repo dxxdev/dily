1530ea48a59fe1fd2334560984ce188c1c258584
silvante <khamidov.ko@gmail.com>
2024-05-02 14:43:41 +0500

    fix

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,38 +6,36 @@ import { logo } from "../assets/images";
 import account_icon from "../assets/icons/profile-icon.svg";
 const Header = ({ top, setTop, one }) => {
   return (
-    <header className="bg-light-gray/80 backdrop-blur-md shadow-sm sticky top-0 z-[9999] py-3">
+    <header className="bg-light-gray/80 backdrop-blur-md shadow-sm sticky top-0 z-[9999] py-1">
       <div className="my-container">
         <div className="flex w-full items-center justify-between">
           <Link>
-            <h1 className="flex items-center gap-1 h-30 font-open-sans">
-              <img src={logo} alt="" className="h-full" />
+            <h1 className="flex items-center gap-1 font-open-sans">
+              <img src={logo} alt="" className="h-full" width={"100px"} />
             </h1>
           </Link>
 
           <nav className="nav">
             <ul className="flex items-center justify-between gap-10">
               <li className="lowercase first-letter:uppercase">
-                <NavLink to='/'>
-                  Internet-magazin Dily.uz
-                </NavLink>
+                <NavLink to="/">Internet-magazin Dily.uz</NavLink>
               </li>
               <li className="lowercase first-letter:uppercase">
-                <NavLink to='/qaysidir_sahifaga_otishini_kimdir_yozib_qoyishi_kerak'>
+                <NavLink to="/qaysidir_sahifaga_otishini_kimdir_yozib_qoyishi_kerak">
                   Markaziy Servis Hizmati
                 </NavLink>
               </li>
               <li className="lowercase first-letter:uppercase">
-                <NavLink to='/qaysidir_sahifaga_otishini_kimdir_yozib_qoyishi_kerak'>
+                <NavLink to="/qaysidir_sahifaga_otishini_kimdir_yozib_qoyishi_kerak">
                   Skupka
                 </NavLink>
               </li>
             </ul>
           </nav>
 
-
-          <Link>
-            <img className="w-5" src={account_icon} alt="" />
+          <Link className=" bg-bright-green rounded-full text-white py-1 px-3 flex items-center">
+            <i className="bx bxs-user text-2xl mr-1"></i>
+            Ro'ihatdan o'tish
           </Link>
         </div>
       </div>
