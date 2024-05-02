3e162380433ef7686f2201c740ef6abe7a275ead
Boburov-Shukurillo <boburovshukurilo@gmail.com>
2024-05-02 16:04:08 +0500

    change 404 not found img

--- client/src/pages/PageNotFound.jsx
+++ client/src/pages/PageNotFound.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import page_not_found_img from '../assets/other-icon/page-not-found-img.png'
-import Header from '../components/Header'
-import CategoryNavLinks from '../components/CategoryNavLinks'
+import page_not_found_img from '../assets/other-icon/page-not-found-img.jpg'
 import greenLine from '../assets/other-icon/line-green.svg'
 import orangeLine from '../assets/other-icon/line-orange.svg'
 const PageNotFoun = ({ one }) => {
@@ -16,7 +14,7 @@ const PageNotFoun = ({ one }) => {
                 <img className='absolute bottom-0 left-1/4 w-40' src={greenLine} alt="" />
                 <img className='absolute top-20 right-0 w-40' src={orangeLine} alt="" />
                 <h1 className='text-2xl text-bright-green font-bold font-open-sans'>Sahifa Topilmadi</h1>
-                <img src={page_not_found_img} alt="page not found img" />
+                <img src={page_not_found_img} alt="page not found img" className='w-1/2' />
                 <Link to='/' className='px-10 py-3 rounded-xl bg-bright-green text-white'>Bosh sahifaga o'tish</Link>
             </div>
         </div>
