d52431f1ef975bacf1e30528e296000833ea3fe3
dxxdev <diyorbekxojamberdiyevn1@gmail.com>
2024-05-05 10:23:15 +0500

    Changes have been made to the Services page

--- client/src/components/ServisesMalfunctions.jsx
+++ client/src/components/ServisesMalfunctions.jsx
@@ -4,23 +4,23 @@ import union from "../assets/svg/Union.svg";
 import { serviseMalfunctionsData } from "../data/ServisesPageData";
 const ServisesMalfunctions = () => {
   const [sliceNumber, setSliceNumber] = useState(18);
-  const sliceFunctions = ()=>{
-    setSliceNumber(serviseMalfunctionsData.length)
-  }
-  const closeFunctions = ()=>{
-    setSliceNumber(18)
-  }
+  const sliceFunctions = () => {
+    setSliceNumber(serviseMalfunctionsData.length);
+  };
+  const closeFunctions = () => {
+    setSliceNumber(18);
+  };
   return (
     <section className="">
       <div className="my-container ">
         <div className="pt-30 pb-10  shadow-product-card-shadow px-68 rounded-10 ">
           <h2 className="font-bold text-[32px] leading-38 text-dark-gray mb-11 text-center">
-            Nosozliklar
+            Tuzata olamiz
           </h2>
           <ul className="grid grid-cols-3 gap-x-[110px] gap-y-[15px] mb-10 ">
             {serviseMalfunctionsData.slice(0, sliceNumber).map((e, index) => {
               return (
-                <li className="flex items-center space-x-3.5">
+                <li key={index} className="flex items-center space-x-3.5">
                   <img src={ServisesWarning} alt="Servise" />
                   <p className="font-medium text-base leading-6 text-dark-gray">
                     {e.title}
@@ -30,10 +30,18 @@ const ServisesMalfunctions = () => {
             })}
           </ul>
           <div className="flex items-center justify-center flex-col">
-            <button onClick={sliceFunctions} className={`${sliceNumber === 18 ? "block" : "hidden"}`}>
+            <button
+              onClick={sliceFunctions}
+              className={`${sliceNumber === 18 ? "block" : "hidden"}`}
+            >
               <img src={union} alt="union" />
             </button>
-            <button onClick={closeFunctions} className={`transform rotate-180 ${sliceNumber === 18 ? "hidden" : "block"}`}>
+            <button
+              onClick={closeFunctions}
+              className={`transform rotate-180 ${
+                sliceNumber === 18 ? "hidden" : "block"
+              }`}
+            >
               <img src={union} alt="union" />
             </button>
           </div>
